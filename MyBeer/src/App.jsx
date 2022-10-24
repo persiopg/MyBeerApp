993f236cd4175f4219c86f8e4fe489657e2e2d3c
persio de paula godoy <40552268+persiopg@users.noreply.github.com>
2022-10-24 13:01:10 -0300

    criação e configuração do app

---
+++ MyBeer/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Rout from "./Rout";
+import "./App.css";
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Rout />
+		</BrowserRouter>
+	);
+}
+
+export default App;
