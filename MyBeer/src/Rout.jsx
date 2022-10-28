a9eceab76986ec04856b30ce0a207f1a6334fdf4
persio de paula godoy <40552268+persiopg@users.noreply.github.com>
2022-10-28 17:35:21 -0300

    1

--- MyBeer/src/Rout.jsx
+++ MyBeer/src/Rout.jsx
@@ -9,7 +9,7 @@ function Rout() {
 	return (
 		<Routes>			
 			<Route path="/" element={<Home />} />
-			<Route path="/user/:userLogin" element={<Home />} />
+			<Route path="/?user/:userLogin" element={<Home />} />
 			<Route path="/user/:userLogin/products/:id" element={<Product />} />
 			<Route path="/acessar" element={<Login />} />
 		</Routes>
